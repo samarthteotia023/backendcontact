0de0241dd074f4fa3f2fabf8220343b16877b208
Samarth Teotia <samarthteotia@Samarth-MacBook-Air.local>
2024-01-09 17:16:08 +0530

    changes

--- middlewares/verifytoken.js
+++ middlewares/verifytoken.js
@@ -1,12 +1,13 @@
 const  {verify} =require( "jsonwebtoken");
 const User = require("../models/user");
 require('dotenv').config()
+const JWT_SECRET="qwert12345"
 const verifyToken =
   async (req, res, next) => {
     let token = req.headers.authorization;
     if (!token) return res.status(401).send({ message: "Unauthorized" });
     let bearer=token.split(" ");
-    let decoded = await verify(bearer[1], process.env.JWT_SECRET)
+    let decoded = await verify(bearer[1], JWT_SECRET)
     req.user=  decoded.user
     next();
   }
--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,11 @@
 var jwt = require('jsonwebtoken');
+
+const JWT_SECRET="qwert12345"
+const JWT_EXPIRE="300000000000000000"
 require('dotenv').config()
 const generateJwtToken = (payload) => {
-    return jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRE,
+    return jwt.sign(payload, JWT_SECRET, {
+      expiresIn: JWT_EXPIRE,
     });
   };
 module.exports=generateJwtToken;
